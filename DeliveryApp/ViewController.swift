7b4cdccf16fe29c49f9fd67923b39e3f996a6c82
Daniil Slaykovski <daniilslaykovski@macbook-pro-daniil.local>
2018-03-25 13:40:44 +0300

    Initial Commit

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by Daniil Slaykovski on 25.03.2018.
+//  Copyright © 2018 Daniil Slaykovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
